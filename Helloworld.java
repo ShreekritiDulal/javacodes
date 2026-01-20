76a8e9e509e68f117d9f6ec49ef7d00b91ec25be
Shreekriti <shreekritidulal13@gmail.com>
2026-01-20 23:55:29 +0530

    Added Multiply program

---
+++ Helloworld.java
@@ -0,0 +1,6 @@
+//HELLO WORLD
+public class Helloworld{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+//addition wo user input
+public class Main{
+    public static void main(String[] args){
+        int num1=10;
+        int num2=20;
+        int sum;
+        sum=num1+num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
